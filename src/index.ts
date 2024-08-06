cac53af83014cf86acbf3d39ad360ac2d7a5fecb
wzc520pyfm <1528857653@qq.com>
2024-08-06 22:06:09 +0800

    feat: add auto-install plugin

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,79 @@
+import * as fs from 'node:fs'
+import * as path from 'node:path'
+import mod from 'node:module'
+import { exec } from 'node:child_process'
+import { promisify } from 'node:util'
 import type { UnpluginFactory } from 'unplugin'
 import { createUnplugin } from 'unplugin'
 import type { Options } from './types'
 
-export const unpluginFactory: UnpluginFactory<Options | undefined> = options => ({
-  name: 'unplugin-starter',
-  transformInclude(id) {
-    return id.endsWith('main.ts')
-  },
-  transform(code) {
-    return code.replace('__UNPLUGIN__', `Hello Unplugin! ${options}`)
-  },
-})
+const execAsync = promisify(exec)
+
+export const unpluginFactory: UnpluginFactory<Options | undefined> = (rawOptions = {}) => {
+  const defaults = {
+    // intentionally undocumented options. used for tests
+    commands: {
+      npm: 'npm install',
+      pnpm: 'pnpm install',
+      yarn: 'yarn add',
+    },
+    manager: fs.existsSync('yarn.lock') ? 'yarn' : fs.existsSync('pnpm-lock.yaml') ? 'pnpm' : 'npm',
+    pkgFile: path.resolve(rawOptions.pkgFile || 'package.json'),
+  }
+
+  const options = Object.assign({}, defaults, rawOptions)
+  const { manager, pkgFile } = options
+  const validManagers = ['npm', 'yarn', 'pnpm']
+
+  if (!validManagers.includes(manager)) {
+    throw new RangeError(
+      `'${manager}' is not a valid package manager. `
+      + `Valid managers include: '${validManagers.join('\', \'')}'.`,
+    )
+  }
+
+  let pkg: any
+  if (fs.existsSync(pkgFile)) {
+    pkg = JSON.parse(fs.readFileSync(pkgFile, 'utf-8'))
+  }
+  else {
+    fs.writeFileSync(pkgFile, '{}')
+    pkg = {}
+  }
+
+  const installed = new Set(Object.keys(pkg.dependencies || {}).concat(mod.builtinModules))
+  const cmd = options.commands[manager]
+
+  return {
+    name: 'unplugin-auto-install',
+    async resolveId(importee, importer) {
+      // entry module
+      if (!importer)
+        return null
+
+      // this function doesn't actually resolve anything, but it provides us with a hook to discover uninstalled deps
+
+      const isExternalPackage
+        = importee[0] !== '.' && importee[0] !== '\0' && !path.isAbsolute(importee)
+
+      if (isExternalPackage) {
+        // we have a bare import — check it's installed
+        const parts = importee.split('/')
+        let name = parts.shift()!
+        if (name[0] === '@')
+          name += `/${parts.shift()}`
+
+        if (!installed.has(name)) {
+          // eslint-disable-next-line no-console
+          console.log(`installing ${name}...`)
+          await execAsync(`${cmd} ${name}`)
+        }
+      }
+
+      return null
+    },
+  }
+}
 
 export const unplugin = /* #__PURE__ */ createUnplugin(unpluginFactory)
 
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,15 @@
 export interface Options {
-  // define your plugin options here
+  /**
+   * Specifies the location on disk of the target `package.json` file.
+   * If the file doesn't exist, it will be created by the plugin,
+   * as package managers need to populate the `dependencies` property.
+   * @default '{cwd}/package.json'
+   */
+  pkgFile?: string
+
+  /**
+   * Specifies the package manager to use; `npm` or `yarn`.
+   * If not specified, the plugin will default to `yarn` if `yarn.lock` exists, or `npm` otherwise.
+   */
+  manager?: 'npm' | 'yarn'
 }
